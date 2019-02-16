910a5b02c8c54bc6905c753c6d086e4f4859c487
Caleb Babin <object.caleb@gmail.com>
2019-02-16 12:35:58 -0600

    adds basic client script

---
+++ client.js
@@ -0,0 +1,51 @@
+let config = {};
+try {
+    config = require(`${__dirname}/config/config.js`);
+} catch (e) {
+    config = require(`${__dirname}/config/config-default.js`);
+}
+
+
+
+
+const WebSocket = require('ws');
+
+const ws = new WebSocket(config.host);
+
+ws.on('open', function open() {
+    console.log('server connection open');
+});
+
+/*ws.on('message', function incoming(data) {
+    console.log(data);  
+});*/
+
+
+
+const midi = require('midi');
+
+const input = new midi.input();
+ 
+// Count the available input ports.
+input.getPortCount();
+ 
+// Get the name of a specified input port.
+input.getPortName(0);
+ 
+// Configure a callback.
+input.on('message', function(deltaTime, message) {
+  // The message is an array of numbers corresponding to the MIDI bytes:
+  //   [status, data1, data2]
+  // https://www.cs.cf.ac.uk/Dave/Multimedia/node158.html has some helpful
+  // information interpreting the messages.
+  console.log('m:' + message + ' d:' + deltaTime);
+
+  ws.send(JSON.stringify({
+      c: 's',
+      channel: 'cool',
+      d: message,
+  }))
+});
+ 
+// Open the first available input port.
+input.openPort(0);
